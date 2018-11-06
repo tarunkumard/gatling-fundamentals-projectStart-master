362d838a057f34f43367af34d56c3b7ddb1b5dc8
Tarun <tarun.kumard@gmail.com>
2018-11-06 07:03:59 -0800

    paramter code

--- scala/MyGatlingRunner.scala
+++ scala/MyGatlingRunner.scala
@@ -1,12 +1,12 @@
 import io.gatling.app.Gatling
 import io.gatling.core.config.GatlingPropertiesBuilder
-import simulations.{CodeReuseWithObjects, Csvfeeder}
+import simulations.{CodeReuseWithObjects, Csvfeeder, RuntimeParameters}
 
 object MyGatlingRunner {
 
   def main(args: Array[String]): Unit = {
 
-    val simClass = classOf[Csvfeeder].getName
+    val simClass = classOf[RuntimeParameters].getName
 
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
--- scala/simulations/RuntimeParameters.scala
+++ scala/simulations/RuntimeParameters.scala
@@ -4,6 +4,7 @@ import baseConfig.BaseSimulation
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ChainBuilder
 import io.gatling.http.Predef._
+import scala.concurrent.duration.DurationInt
 
 class RuntimeParameters extends BaseSimulation{
 
@@ -13,6 +14,17 @@ class RuntimeParameters extends BaseSimulation{
         .get("videogames")
         .check(status.is(expected = 200))    )
   }
+
+  val scn = scenario(scenarioName = "Video Game DB")
+      .forever(){
+        exec(getAllvideoGames())
+              }
+setUp(
+  scn.inject(
+    nothingFor(5 seconds),
+    rampUsers(users =1) over(1 second))
+  )
+    .protocols(httpConf).maxDuration(duration = 20 seconds)
 
 
 
